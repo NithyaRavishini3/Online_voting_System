d4ecab6ed4d4c8cd81a68397c05540e856ba522a
Nithya Ravishini <125907269+NithyaRavishini3@users.noreply.github.com>
2025-06-14 16:14:54 +0530

    Add files via upload

---
+++ panoating.js
@@ -0,0 +1,20 @@
+01// JavaScript source code
+function myFunction() {
+
+    if (document.getElementById("pwd").value != document.getElementById("Repwd").value) {
+        alert("Password is not match");
+        return false;
+    } else {
+        alert("Success");
+        return true;
+    }
+
+
+
+}
+
+
+
+
+
+
